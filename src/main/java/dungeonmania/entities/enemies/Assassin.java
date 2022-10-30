4ad321a6f87cb04da5609332a90c43e1111086ac
Ka Meng Lai <ka.m.lai@student.unsw.edu.au>
2022-10-30 19:59:48 +1100

    Task2aiii Assassin

--- src/main/java/dungeonmania/entities/enemies/Assassin.java
+++ src/main/java/dungeonmania/entities/enemies/Assassin.java
@@ -1,5 +1,7 @@
 package dungeonmania.entities.enemies;
 
+import java.util.Random;
+
 import dungeonmania.Game;
 import dungeonmania.entities.Bribe;
 import dungeonmania.entities.Bribeable;
@@ -9,21 +11,26 @@ import dungeonmania.entities.Player;
 import dungeonmania.map.GameMap;
 import dungeonmania.util.Position;
 
-public class Mercenary extends Enemy implements Interactable, Bribeable {
+public class Assassin extends Enemy implements Interactable, Bribeable {
     public static final int DEFAULT_BRIBE_AMOUNT = 1;
     public static final int DEFAULT_BRIBE_RADIUS = 1;
     public static final double DEFAULT_ATTACK = 5.0;
     public static final double DEFAULT_HEALTH = 10.0;
+    public static final double DEFAULT_BRIBE_FAIL_RATE = 0.3;
 
-    private int bribeAmount = Mercenary.DEFAULT_BRIBE_AMOUNT;
-    private int bribeRadius = Mercenary.DEFAULT_BRIBE_RADIUS;
+    private int bribeAmount = Assassin.DEFAULT_BRIBE_AMOUNT;
+    private int bribeRadius = Assassin.DEFAULT_BRIBE_RADIUS;
+    private double bribeFailRate = Assassin.DEFAULT_BRIBE_FAIL_RATE;
     private boolean allied = false;
     private Bribe bribe = new Bribe();
+    private Random random = new Random(System.currentTimeMillis());
 
-    public Mercenary(Position position, double health, double attack, int bribeAmount, int bribeRadius) {
+    public Assassin(Position position, double health, double attack,
+            int bribeAmount, int bribeRadius, double bribeFailRate) {
         super(position, health, attack);
         this.bribeAmount = bribeAmount;
         this.bribeRadius = bribeRadius;
+        this.bribeFailRate = bribeFailRate;
     }
 
     public boolean isAllied() {
@@ -38,9 +45,15 @@ public class Mercenary extends Enemy implements Interactable, Bribeable {
 
     @Override
     public void interact(Player player, Game game) {
-        allied = true;
         bribe.bribe(player, this);
+        if (random.nextInt(10) <= bribeFailRate * 10) {
+            // fails
+            allied = false;
+        } else {
+            allied = true;
+        }
     }
+
 
     @Override
     public void move(Game game) {
@@ -61,4 +74,5 @@ public class Mercenary extends Enemy implements Interactable, Bribeable {
     public int getBribeAmount() {
         return bribeAmount;
     }
+
 }
